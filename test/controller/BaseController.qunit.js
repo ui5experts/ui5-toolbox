0b4f93577d979a7d15254b9b641a7507d3d92ce7
Christian Schuff <christian.schuff@gmail.com>
2016-06-23 14:36:55 +0200

    first qunit test

---
+++ test/controller/BaseController.qunit.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+  "sap/ui/qunit/qunit-css",
+  "sap/ui/qunit/qunit-junit",
+  "sap/ui/qunit/qunit-coverage",
+  "ui5experts/toolbox/controller/BaseController"
+], function () {
+
+  "use strict";
+
+
+
+});
