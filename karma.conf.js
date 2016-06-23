b89a1d36c3b8729fbf579ec3932823df17ecf58e
Christian Schuff <christian.schuff@gmail.com>
2016-06-23 16:07:11 +0200

    adds code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,13 @@ module.exports = function(config) {
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['qunit', 'openui5'],
-    plugins: ['karma-qunit', 'karma-openui5', 'karma-chrome-launcher'],
+
+    plugins: [
+      'karma-qunit',
+      'karma-openui5',
+      'karma-chrome-launcher',
+      'karma-coverage'
+    ],
 
 
     openui5: {
@@ -67,13 +73,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/**/*.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
 
 
     // web server port
