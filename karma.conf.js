1213d70c2186aa4c43e03dfcab7bf3ca321fc6b0
Christian Schuff <christian.schuff@gmail.com>
2016-06-23 14:36:31 +0200

    karma setup

---
+++ karma.conf.js
@@ -0,0 +1,97 @@
+// Karma configuration
+// Generated on Thu Jun 23 2016 14:05:57 GMT+0200 (CEST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['qunit', 'openui5'],
+    plugins: ['karma-qunit', 'karma-openui5', 'karma-chrome-launcher'],
+
+
+    openui5: {
+      path: 'https://openui5.hana.ondemand.com/resources/sap-ui-core.js',
+      useMockServer: false
+    },
+
+    client: {
+      openui5: {
+        config: {
+          theme: 'sap_bluecrystal',
+          resourceRoots: {
+            "ui5experts.toolbox": '/'
+          }
+        }
+//        mockserver: {
+//          config: {
+//            autoRespond: true
+//          },
+//          rootUri: '/my/service/',
+//          metadataURL: '/base/test/mock.xml',
+//          mockdataSettings: {
+//
+//          }
+//        }
+      }
+    },
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
